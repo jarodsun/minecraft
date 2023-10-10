9dab690d82e0f2c68eff60c91af9d7c32fd9a60b
jarod <garsday@gmail.com>
2023-10-10 17:31:23 +0800

    添加“回家”命令

---
+++ mods/trymodding/src/main/java/com/jarodsun/trymodding/events/ModEvents.java
@@ -0,0 +1,30 @@
+package com.jarodsun.trymodding.events;
+
+import com.jarodsun.trymodding.TryModding;
+import com.jarodsun.trymodding.commands.ReturnHomeCommand;
+import com.jarodsun.trymodding.commands.SetHomeCommand;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.server.command.ConfigCommand;
+
+@Mod.EventBusSubscriber(modid = TryModding.MOD_ID)
+public class ModEvents {
+
+    @SubscribeEvent
+    public static void onCommandsRegister(RegisterCommandsEvent event) {
+        new SetHomeCommand(event.getDispatcher());
+        new ReturnHomeCommand(event.getDispatcher());
+
+        ConfigCommand.register(event.getDispatcher());
+    }
+
+    @SubscribeEvent
+    public static void onPlyaerCloneEvent(PlayerEvent.Clone event) {
+        if (!event.getOriginal().getEntityWorld().isRemote()) {
+            event.getPlayer().getPersistentData().putIntArray(TryModding.MOD_ID + "homepos",
+            event.getOriginal().getPersistentData().getIntArray(TryModding.MOD_ID + "homepos"));
+        }
+    }
+}
