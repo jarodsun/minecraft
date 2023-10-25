ce4ce4b876d674b02cd38545f9754dd084e3fe96
jarod <garsday@gmail.com>
2023-10-26 01:49:07 +0800

    添加飞行戒指，带夜视效果。

--- mods/awsomeworld/src/main/java/com/jarod/awsomeworld/item/ModItems.java
+++ mods/awsomeworld/src/main/java/com/jarod/awsomeworld/item/ModItems.java
@@ -1,14 +1,20 @@
 package com.jarod.awsomeworld.item;
 
 import com.jarod.awsomeworld.AwsomeWorld;
+import com.jarod.awsomeworld.item.custom.RingFlight;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, AwsomeWorld.MOD_ID);
+
+    public static final RegistryObject<Item> RINGFLIGHT = ITEMS.register("ring_flight",
+            () -> new RingFlight(new Item.Properties().group(ModItemGroup.AWSOME_WORLD)));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
